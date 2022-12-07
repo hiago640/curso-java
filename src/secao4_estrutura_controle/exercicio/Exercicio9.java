d6b9b5f5df38691c6f5e7857d028d390fe47e815
hiago640 <hiago_pereiraborges@hotmail.com>
2022-12-07 15:36:03 -0300

    feat: incluindo exercicio 9

--- src/secao4_estrutura_controle/exercicio/Exercicio9.java
+++ src/secao4_estrutura_controle/exercicio/Exercicio9.java
@@ -1,5 +1,7 @@
 package secao4_estrutura_controle.exercicio;
 
+import java.util.Scanner;
+
 public class Exercicio9 {
 
 	/**
@@ -7,6 +9,24 @@ public class Exercicio9 {
 	 */
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int maiorValor = 0;
+		int contador = 0;
+
+		do {
+			System.out.println("\nDigite um numero: ");
+			int valor = scanner.nextInt();
+
+			if (valor > maiorValor) {
+				maiorValor = valor;
+			}
+
+			contador++;
+		} while (contador != 10);
+
+		System.out.printf("O maior valor foi: " + maiorValor);
+		scanner.close();
 
 	}
 
