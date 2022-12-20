3183c7a4b1a9af179bf961c6e0e470f6eb8d5f12
hiago640 <hiago_pereiraborges@hotmail.com>
2022-12-20 08:56:28 -0300

    feat: continuando curso

---
+++ src/secao6_arrays_collections/curso/arrays/DesafioArray.java
@@ -0,0 +1,33 @@
+package secao6_arrays_collections.curso.arrays;
+
+import java.util.Scanner;
+
+public class DesafioArray {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Bem vindo ao sistema de gerenciamento de Notas.");
+
+		System.out.print("Quantas notas deseja cadastrar?: ");
+		int qtdNotas = Integer.parseInt(entrada.nextLine());
+
+		double[] notas = new double[qtdNotas];
+		
+		System.out.println();
+		for (int i = 0; i < notas.length; i++) {
+			System.out.print("Insira a " + (i + 1) + "º nota: ");
+			notas[i] = Double.parseDouble(entrada.nextLine());
+		}
+
+		System.out.println("\nCalculando média...\n");
+
+		double total = 0;
+		for (double nota : notas)
+			total += nota;
+
+		System.out.printf("Média calculada: %s", total / notas.length);
+
+		entrada.close();
+	}
+}
---
+++ src/secao6_arrays_collections/curso/arrays/ForEach.java
@@ -0,0 +1,17 @@
+package secao6_arrays_collections.curso.arrays;
+
+public class ForEach {
+	
+	public static void main(String[] args) {
+		
+		double[] notas = new double[3];
+		
+		notas[0] = 7.9;
+		notas[1] = 8;
+		notas[2] = 6.7;
+		
+		for(double nota : notas)
+			System.out.println(nota);
+		
+	}
+}
