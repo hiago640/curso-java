4418621b2b806d251e2dd0accd25639b4125a511
hiago640 <hiago_pereiraborges@hotmail.com>
2023-04-11 23:38:39 -0300

    feat: continuidade secao8 lambda

---
+++ src/secao8_lambda/Fornecedor.java
@@ -0,0 +1,18 @@
+package secao8_lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Fornecedor {
+
+    public static void main(String[] args) {
+
+        // nao recebe nenhum argumento, mas retorna algo
+        Supplier<List<String>> lista = () -> Arrays.asList("Ana", "Bia", "Gui");
+
+        System.out.println(lista.get());
+
+    }
+
+}
--- src/secao8_lambda/interface_funcional/Calculo.java
+++
@@ -1,5 +0,0 @@
-package secao8_lambda.interface_funcional;
-
-public interface Calculo {	
-	public double executar(double a, double b);
-}
